525396c9d7e2a649b184b03a28451a93c682adb9
Harangozó Martin <harangozom97@gmail.com>
2021-11-26 12:40:37 +0100

    Add base project

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+import { Button, Grid } from '@material-ui/core';
+import React from 'react';
+
+export default function MainPage() {
+  return (
+    <Grid>
+      <Button>Asd</Button>
+    </Grid>
+  );
+}
